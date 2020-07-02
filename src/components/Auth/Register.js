aa033ca89f26c4422960124de63b1b25ded8d063
Mateo <rosenmatt1@gmail.com>
2020-07-02 16:32:48 -0600

    Finished Register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -29,19 +29,32 @@ const Register = ({ classes }) => {
           Register
         </Typography>
 
-        <Mutation>
+        {/* <Mutation>
           {() => {
-
-            return (
+            return ( */}
               <form className={classes.form}>
                 <FormControl margin="normal" required fullWidth>
                   <InputLabel htmlFor="username"> Username </InputLabel>
                   <Input id="username" />
-                  </FormControl >
-                </form>
-            )
+                </FormControl >
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="email"> Email </InputLabel>
+                  <Input id="email" type="email" />
+                </FormControl >
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="password"> Password </InputLabel>
+                  <Input id="password" type="password" />
+                </FormControl >
+                <Button type="submit" fullWidth variant="contained" color="secondary" className={classes.submit}>
+                  Register
+                </Button>
+                <Button  fullWidth color="primary" variant="outlined">
+                  Previous user? Log in here
+                </Button>
+              </form>
+            {/* )
           }}
-        </Mutation>
+        </Mutation> */}
 
       </Paper>
     </div>
@@ -49,42 +62,42 @@ const Register = ({ classes }) => {
 };
 
 const styles = theme => ({
-        root: {
-        width: "auto",
+  root: {
+    width: "auto",
     display: "block",
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up("md")]: {
-        width: 400,
+      width: 400,
       marginLeft: "auto",
       marginRight: "auto"
     }
   },
   paper: {
-        marginTop: theme.spacing.unit * 8,
+    marginTop: theme.spacing.unit * 8,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     padding: theme.spacing.unit * 2
   },
   title: {
-        marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 2,
     color: theme.palette.openTitle
   },
   avatar: {
-        margin: theme.spacing.unit,
+    margin: theme.spacing.unit,
     backgroundColor: theme.palette.secondary.main
   },
   form: {
-        width: "100%",
+    width: "100%",
     marginTop: theme.spacing.unit
   },
   submit: {
-        marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2
   },
   icon: {
-        padding: "0px 2px 2px 0px",
+    padding: "0px 2px 2px 0px",
     verticalAlign: "middle",
     color: "green"
   }
