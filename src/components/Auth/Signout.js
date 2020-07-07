6f9d0f00dd7d67c5b7e27b0b90e3f32ae5ce7a7a
Mateo <rosenmatt1@gmail.com>
2020-07-07 13:35:20 -0600

    Made signout button remove localstorage.  Also added client into Signout

--- src/components/Auth/Signout.js
+++ src/components/Auth/Signout.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { ApolloConsumer } from 'react-apollo';  //way to get access to client
 import withStyles from "@material-ui/core/styles/withStyles";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
 const Signout = ({ classes }) => {
+  const handleSignout = client => {
+    localStorage.removeItem('authToken')
+    client.writeData({data: { isLoggedIn: false } })
+    console.log("Sined out user", client)
+  }
+
   return (
-    <button>
-      <Typography variant="body1" className={classes.buttonText} color="secondary" noWrap>
-        Signout
+    <ApolloConsumer>
+      {client => (
+        <Button onClick={() => handleSignout(client)}>
+          <Typography variant="body1" className={classes.buttonText} color="secondary" noWrap>
+            Signout
       </Typography>
-      <ExitToApp className={classes.buttonIcon} color="secondary" />
-    </button>
+          <ExitToApp className={classes.buttonIcon} color="secondary" />
+        </Button>
+      )}
+    </ApolloConsumer>
   )
 };
 
