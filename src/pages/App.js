a78111afee15358fe1578afeafa64dd2d269d4ef
Mateo <rosenmatt1@gmail.com>
2020-07-08 12:26:20 -0600

    Created the bones for the App main page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
 import withStyles from "@material-ui/core/styles/withStyles";
 
+import SearchTracks from '../components/Track/SearchTracks';
+import TrackList from '../components/Track/TrackList';
+import CreateTrack from '../components/Track/CreateTrack';
+
+
 const App = ({ classes }) => {
-  return <div>App</div>;
+  return (
+  <div className={classes.container}>
+    <SearchTracks />
+    <CreateTrack />
+    <TrackList />
+
+  </div>
+    )
 };
 
 const styles = theme => ({
